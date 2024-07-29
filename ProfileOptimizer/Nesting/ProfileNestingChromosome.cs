6764b560f0f24d9019978f0e879dc03db323643c
锋 杨 <yangf85@163.com>
2024-07-29 15:14:41 +0800

    修改

---
+++ ProfileOptimizer/Nesting/ProfileNestingChromosome.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GeneticSharp;
+
+namespace ProfileOptimizer.Nesting;
+
+public class ProfileNestingChromosome : ChromosomeBase
+{
+    public ProfileNestingChromosome(int numberOfParts) : base(numberOfParts)
+    {
+        // Create genes with random sequence of parts
+        for (int i = 0; i < numberOfParts; i++)
+        {
+            ReplaceGene(i, new Gene(i));
+        }
+    }
+
+    public override IChromosome CreateNew()
+    {
+        return new ProfileNestingChromosome(Length);
+    }
+
+    public override Gene GenerateGene(int geneIndex)
+    {
+        return new Gene(geneIndex);
+    }
+
+    public override IChromosome Clone()
+    {
+        var clone = base.Clone() as ProfileNestingChromosome;
+        return clone;
+    }
+
+    public int[] GetPartSequence()
+    {
+        return GetGenes().Select(g => (int)g.Value).ToArray();
+    }
+}
